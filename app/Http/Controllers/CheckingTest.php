20e4b3aadc97e25a05663af57f6804a04a01fdc8
shahajalalweb <shahajalalbadsha5@gmail.com>
2024-09-21 16:46:03 +0600

    learning laravel crud

---
+++ app/Http/Controllers/CheckingTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CheckingTest extends Controller
+{
+    public function Checking() {
+        return "Hello Word";
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\CheckingTest;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::get('/check', [CheckingTest::class, 'Checking']);
+Route::get('/user', [UserController::class,'index'])->name('user');
+Route::post('/user/add', [UserController::class,'store'])->name('user.add');
